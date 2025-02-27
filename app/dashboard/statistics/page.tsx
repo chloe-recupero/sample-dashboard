753662ad178c1e88da3a22c3d36e2d2c8973a103
Chloe Recupero <chloe@bluco.ai>
2025-02-27 14:46:01 +0100

    First

---
+++ app/dashboard/statistics/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return(
+    <div>
+        <p className="text-base">Statistics Page</p>
+        <h1 className="text-3xl font-bold">Hello Chloe,</h1>
+    </div>
+        
+    );
+  }
